45ad9813ba8b3b4d6dc3154e3064948ab8534ae8
Moop204 <andrew.nguyen2045@outlook.com>
2022-01-10 08:03:44 +1100

    [feat] Give component access to default component props and padding for controls

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
-import { Box } from "@mui/system";
+import { DefaultComponentProps } from "@mui/material/OverridableComponent";
+import { Box, BoxTypeMap } from "@mui/system";
 import { motion } from "framer-motion";
 import { FunctionComponent, MouseEventHandler, useState } from "react";
 import { LeftRotate, RightRotate } from "./Controls";
@@ -10,24 +11,29 @@ import nice from "./media/nice.png";
 import peace from "./media/peace.png";
 import spooopiwer from "./media/spooopiwer.png";
 
-interface CarouselProp {
+interface CarouselProp extends DefaultComponentProps<BoxTypeMap<{}, "div">> {
   height: number;
   width: number;
   gap: number;
-  padding: number;
   contents: string[];
+  // padding?: number;
+  innerBorderRadius?: string;
+  outerBorderRadius?: string;
+  outerBackgroundColor?: string;
+  controlBackgroundColor?: string;
 }
 
-const Carousel: FunctionComponent<CarouselProp> = (
-  {
+const Carousel: FunctionComponent<CarouselProp> = (prop) => {
+  const {
     width = 300,
     height = 100,
     gap = 10,
     contents = [creepy, dancin, spooopiwer, peace, layingdown, nice],
-    padding = 4,
-  },
-  prop
-) => {
+    innerBorderRadius = "50%",
+    outerBorderRadius = "0px",
+    outerBackgroundColor = "none",
+  } = prop;
+
   const boxWidth =
     (width - gap * (contents.length - 3)) / (contents.length - 2);
   const positions: number[] = [];
@@ -35,23 +41,15 @@ const Carousel: FunctionComponent<CarouselProp> = (
   for (let i = 0; i < imgNum; i++) {
     positions.push((boxWidth + gap) * i);
   }
-
   const [pos, setPos] = useState(0);
-  // const [leftHide, setLeftHide] = useState(0);
-  // const [rightHide, setRightHide] = useState(contents.length - 1);
-  const limit = (boxWidth + gap) * contents.length;
 
   const rotateRight = () => {
-    const change = boxWidth + gap;
     setPos((pos + 1 + contents.length) % contents.length);
   };
 
   const rotateLeft = () => {
     setPos((pos - 1 + contents.length) % contents.length);
   };
-
-  console.log(positions);
-  console.log(pos);
 
   const isOpaque = (p: number) => {
     if (p == 0 || p == contents.length - 1) {
@@ -62,50 +60,60 @@ const Carousel: FunctionComponent<CarouselProp> = (
 
   return (
     <>
-      <Box
-        position="relative"
-        width={width}
-        height={height}
-        bgcolor={"red"}
-        padding={padding + "px"}
-        {...prop}
-      >
-        {contents.map((bg, idx) => {
-          let posIdx = (pos + idx + contents.length) % contents.length;
-          const ogIdx = posIdx;
-          if (posIdx == contents.length - 1) {
-            posIdx = contents.length - 3;
-          } else if (posIdx != 0) {
-            posIdx--;
-          }
-          console.log(idx + " -> " + posIdx);
-          console.log(idx + " -> " + ogIdx);
-
-          return (
-            <motion.div
-              animate={{
-                x: positions[posIdx],
-                opacity: isOpaque(ogIdx) ? 1 : 0.0,
-              }}
-            >
-              <Box position="absolute" height={height} width={boxWidth}>
-                <img src={bg} alt="fireSpot" height="100%" width="100%" />
-              </Box>
-            </motion.div>
-          );
-        })}
+      <Box display="flow" width={width}>
+        <Box
+          position="relative"
+          width={width}
+          height={height}
+          bgcolor={outerBackgroundColor}
+          borderRadius={outerBorderRadius}
+          {...prop}
+        >
+          {contents.map((bg, idx) => {
+            let posIdx = (pos + idx + contents.length) % contents.length;
+            const ogIdx = posIdx;
+            if (posIdx == contents.length - 1) {
+              posIdx = contents.length - 3;
+            } else if (posIdx != 0) {
+              posIdx--;
+            }
+            return (
+              <motion.div
+                animate={{
+                  x: positions[posIdx],
+                  opacity: isOpaque(ogIdx) ? 1 : 0.0,
+                }}
+              >
+                <Box
+                  position="absolute"
+                  height={height}
+                  width={boxWidth}
+                  overflow={"hidden"}
+                  sx={{
+                    borderRadius: innerBorderRadius,
+                  }}
+                >
+                  <img src={bg} alt="fireSpot" height="100%" width="100%" />
+                </Box>
+              </motion.div>
+            );
+          })}
+        </Box>
+        <Box
+          width={"100%"}
+          sx={{
+            // width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            paddingLeft: prop.paddingLeft ? prop.paddingLeft : prop.padding,
+            paddingRight: prop.paddingLeft ? prop.paddingRight : prop.padding,
+          }}
+        >
+          <LeftRotate onClick={rotateLeft} />
+          <RightRotate onClick={rotateRight} />
+        </Box>
       </Box>
-      <Box
-        width={width}
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <LeftRotate onClick={rotateLeft} />
-        <RightRotate onClick={rotateRight} />
-      </Box>
-      {/* <image src="https://upload.wikimedia.org/wikipedia/commons/8/8a/P%26W_4006_Baltic_CT.jpg" /> */}
     </>
   );
 };
